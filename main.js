633860dd7a43e8d7d0fccfe987b83f253df5732e
Evgeniy Talko <etalko@gmail.com>
2021-04-07 18:49:36 +0300

    refactor(main.js): add fn createElement

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const player1 = {
-  name: "Scorpion",
+  name: "SCORPION",
   hp: 80,
   img: "http://reactmarathon-api.herokuapp.com/assets/scorpion.gif",
   weapon: "harpoon",
@@ -7,26 +7,27 @@ const player1 = {
 }
 
 const player2 = {
-  name: "Sonya",
+  name: "SONYA",
   hp: 100,
   img: "http://reactmarathon-api.herokuapp.com/assets/sonya.gif",
   weapon: "gun",
   attack: () => console.log(this.name + " Fight..."),
 }
 
+function createElement(htmlTag, classname) {
+  const el = document.createElement(htmlTag);
+  if (classname) el.classList.add(classname);
+  return el;
+}
+
 function createPlayer(classname, playerObj) {
   const arenas = document.querySelector(".arenas")
-  const player = document.createElement("div");
-  player.classList.add(`${classname}`);
-  const progressbar = document.createElement("div");
-  progressbar.classList.add("progressbar");
-  const life = document.createElement("div");
-  life.classList.add("life");
-  const name = document.createElement("div");
-  name.classList.add("name");
-  const character = document.createElement("div");
-  character.classList.add("character");
-  const img = document.createElement("img");
+  const player = createElement("div", classname);
+  const progressbar = createElement("div", "progressbar");
+  const life = createElement("div", "life");
+  const name = createElement("div", "name");
+  const character = createElement("div", "character");
+  const img = createElement("img");
 
   img.src = playerObj.img;
   life.style.width = `${playerObj.hp}%`
